879ad46c380465b9e509e99c0534e5b703327ece
matehth <mate.cimbi@gmail.com>
2025-07-09 20:57:54 +0200

    prints ascii image

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from PIL import Image
 
-
-def read_image(image_file):
-    im = Image.open(image_file)
-    return im
+ASCII_CHARS = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+MAX_BRIGHTNESS = 255
 
 
 def get_pixel_matrix(image):
@@ -11,11 +9,45 @@ def get_pixel_matrix(image):
     return [pixels[i:i + image.width] for i in range(0, len(pixels), image.width)]
 
 
+def get_brightness_matrix(image):
+    pixel_matrix = get_pixel_matrix(image)
+
+    brightness_matrix = []
+    for x in range(len(pixel_matrix)):
+        col = []
+        for y in range(len(pixel_matrix[x])):
+            col.append(round((pixel_matrix[x][y][0] +
+                              pixel_matrix[x][y][1] +
+                              pixel_matrix[x][y][2]) / 3))
+        brightness_matrix.append(col)
+
+    return brightness_matrix
+
+
+def get_char_matrix(image):
+    brightness_matrix = get_brightness_matrix(image)
+
+    qt = round(MAX_BRIGHTNESS / len(ASCII_CHARS))
+
+    char_matrix = []
+    for x in range(len(brightness_matrix)):
+        col = []
+        for y in range(len(brightness_matrix[x])):
+            col.append(ASCII_CHARS[round(brightness_matrix[x][y] / qt)])
+        char_matrix.append(col)
+
+    return char_matrix
+
+
+def print_ascii(image):
+    char_matrix = get_char_matrix(image)
+
+    for x in range(len(char_matrix)):
+        for y in range(len(char_matrix[x])):
+            print(char_matrix[x][y], end='')
+        print("")
+
+
 if __name__ == "__main__":
-
-    car = read_image("car.jpg")
-    print(f"Image size: {car.width} x {car.height}")
-
-    car_pixel_matrix = get_pixel_matrix(car)
-    print(car_pixel_matrix[0][0])
-
+    car_image = Image.open("car.jpg")
+    print_ascii(car_image)
