8f45a1f1b3533ca8e6f60d472dd4148160f14dc3
matehth <mate.cimbi@gmail.com>
2025-07-10 19:26:38 +0200

    fixed images being squashed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PIL import Image
 
-ASCII_CHARS = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+ASCII_CHARS_LONG = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+ASCII_CHARS_SHORT = ".:-=+*#%@"
 MAX_BRIGHTNESS = 255
 
 
@@ -24,23 +25,42 @@ def get_brightness_matrix(image):
     return brightness_matrix
 
 
-def get_char_matrix(image):
+def invert_brightness(image):
     brightness_matrix = get_brightness_matrix(image)
 
-    qt = round(MAX_BRIGHTNESS / len(ASCII_CHARS))
+    inverted_matrix = []
+    for x in range(len(brightness_matrix)):
+        col = []
+        for y in range(len(brightness_matrix[x])):
+            col.append(abs(brightness_matrix[x][y] - 255))
+        inverted_matrix.append(col)
+
+    return inverted_matrix
+
+
+def get_char_matrix(image, char_set, inverted=False):
+    if inverted:
+        brightness_matrix = invert_brightness(image)
+    else:
+        brightness_matrix = get_brightness_matrix(image)
+
+    qt = round(MAX_BRIGHTNESS / len(char_set))
 
     char_matrix = []
     for x in range(len(brightness_matrix)):
         col = []
         for y in range(len(brightness_matrix[x])):
-            col.append(ASCII_CHARS[round(brightness_matrix[x][y] / qt)])
+            col.append(char_set[round(brightness_matrix[x][y] / qt)] * 3)
         char_matrix.append(col)
 
     return char_matrix
 
 
-def print_ascii(image):
-    char_matrix = get_char_matrix(image)
+def print_ascii(image, char_set, inverted=False):
+    if inverted:
+        char_matrix = get_char_matrix(image, char_set, True)
+    else:
+        char_matrix = get_char_matrix(image, char_set)
 
     for x in range(len(char_matrix)):
         for y in range(len(char_matrix[x])):
@@ -49,5 +69,8 @@ def print_ascii(image):
 
 
 if __name__ == "__main__":
-    car_image = Image.open("car.jpg")
-    print_ascii(car_image)
+    img = Image.open("aga.jpg")
+    img.thumbnail((40, 40))
+
+    # Only works with the long char set
+    print_ascii(img, ASCII_CHARS_LONG)
